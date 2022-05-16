8eed642a9e1ee70b3cd282eb4fd2bf19932a1d9d
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 14:36:54 +0200

    feat: Add Sidebar

---
+++ src/containers/Sidebar/Sidebar.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: white;
+  padding: 0 1em;
+  min-height: 100vh;
+`;
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper } from './Sidebar.styles';
+
+const Sidebar = () => {
+  return (
+    <Wrapper>
+      <h1>hello</h1>
+    </Wrapper>
+  );
+};
+
+export default Sidebar;
