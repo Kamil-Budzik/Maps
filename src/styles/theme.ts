6b98a5a93b4dca09d3ca95505e3268c0451037ef
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 15:05:01 +0200

    chore: Add PDF button

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,9 @@ import { DefaultTheme } from 'styled-components';
 const theme: DefaultTheme = {
   colors: {
     primary: '#AEBBFF',
+    primaryHover: '#AEBBFF',
     red: '#F40F02',
+    redHover: '#b70b01',
   },
 };
 
