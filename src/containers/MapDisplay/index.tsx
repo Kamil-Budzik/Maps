270502101a022f5616b752a6efadf73b11ccacb8
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 15:12:14 +0200

    chore: Add responsive styles

---
+++ src/containers/MapDisplay/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper } from './MapDisplay.styles';
+
+const MapDisplay = () => {
+  return (
+    <Wrapper>
+      <h2>MAP DISPLAY</h2>
+    </Wrapper>
+  );
+};
+
+export default MapDisplay;
--- src/pages/Map/Map.styles.ts
+++ src/pages/Map/Map.styles.ts
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.main`
   background-color: ${({ theme }) => theme.colors.primary};
+
+  @media (min-width: 800px) {
+    display: flex;
+  }
 `;
