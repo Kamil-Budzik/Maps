0acf36c2fa7e9667c70598e6d2af78f2c23cde69
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 18:08:00 +0200

    chore: Remove leaflet-bar

--- src/containers/MapDisplay/MapDisplay.styles.ts
+++ src/containers/MapDisplay/MapDisplay.styles.ts
@@ -1,4 +1,20 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  width: 100%;
+  height: 100vh;
+  padding: 4em 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 800px) {
+    padding: 4em 2em;
+    width: calc(100% - 350px);
+  }
+
+  .leaflet-bar {
+    display: none !important;
+  }
+`;
 
