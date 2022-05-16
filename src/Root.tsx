5bc25a1a2b95aa9990469053df1d86ea9760d78b
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 14:07:14 +0200

    feat: Add app routing

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import GlobalStyle from 'styles/globalStyle';
 import theme from 'styles/theme';
 
+import Homepage from 'pages/Homepage/Homepage';
+import History from 'pages/History/History';
+import Map from 'pages/Map/Map';
+
 const Root = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <h1>Hello</h1>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
