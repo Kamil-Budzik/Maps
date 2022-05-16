9b6dd0da6b94c3e4a1af6d4573d7cae0c94e0a02
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 14:51:54 +0200

    chore: Add Input component

--- src/containers/Sidebar/Sidebar.styles.ts
+++ src/containers/Sidebar/Sidebar.styles.ts
@@ -2,6 +2,22 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   background: white;
-  padding: 0 1em;
+  padding: 0 2em;
   min-height: 100vh;
+
+  .inputWrapper:first-child {
+    margin-bottom: 2em;
+  }
+`;
+
+export const Label = styled.label`
+  display: block;
+  font-weight: 500;
+  margin-bottom: 1em;
+`;
+
+export const Subtitle = styled.h2`
+  font-weight: 600;
+  font-size: 2rem;
+  margin-bottom: 1em;
 `;
