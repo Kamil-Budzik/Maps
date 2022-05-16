ab658ef5690cae4627140a21b21c111ee2a9e501
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 18:08:13 +0200

    chore: Display map on the page

--- src/containers/MapDisplay/index.tsx
+++ src/containers/MapDisplay/index.tsx
@@ -1,11 +1,29 @@
+import RoutingMachine from 'components/RoutingMachine/RoutingMachine';
 import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 import { Wrapper } from './MapDisplay.styles';
 
 const MapDisplay = () => {
   return (
     <Wrapper>
-      <h2>MAP DISPLAY</h2>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <RoutingMachine />
+      </MapContainer>
     </Wrapper>
   );
 };
