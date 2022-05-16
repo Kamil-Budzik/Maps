7c585ca46c00e38483e18b4deeaf0f754ac7e577
DonquixoteSabo <mixboksu@gmail.com>
2022-05-16 14:00:05 +0200

    chore: Add theme

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from 'styles/globalStyle';
+import theme from 'styles/theme';
 
 const Root = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <h1>Hello</h1>
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#AEBBFF',
+    red: '#F40F02',
+  },
+};
+
+export default theme;
