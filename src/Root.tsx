d14285e0738697ca4c733c58f46a5a238fe7f48e
DonquixoteSabo <mixboksu@gmail.com>
2022-05-17 14:35:16 +0200

    chore: Setup locationSlice with redux-toolkit

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from 'styles/globalStyle';
 import theme from 'styles/theme';
@@ -9,17 +10,21 @@ import Homepage from 'pages/Homepage';
 import History from 'pages/History';
 import Map from 'pages/Map';
 
+import { store } from 'store';
+
 const Root = () => {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   );
 };
