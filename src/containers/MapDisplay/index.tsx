a03c5a8b87b5f251fcd0624421da19cadc0f1228
DonquixoteSabo <mixboksu@gmail.com>
2022-05-17 18:44:28 +0200

    chore: Render road on the map

--- src/containers/MapDisplay/index.tsx
+++ src/containers/MapDisplay/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { TileLayer } from 'react-leaflet';
+import { useSelector } from 'react-redux';
 
 import RoutingMachine from 'components/RoutingMachine';
+import { RootState } from 'store';
 
 import { StyledMapContainer, Wrapper } from './MapDisplay.styles';
 
 const MapDisplay = () => {
+  const { startingCityLoc, destinationCityLoc } = useSelector(
+    (state: RootState) => state.location
+  );
 
   return (
     <Wrapper>
       <StyledMapContainer
-        center={[51.505, -0.09]}
+        center={[startingCityLoc[0], startingCityLoc[1]]}
         zoom={13}
         scrollWheelZoom={true}
         style={{ width: '100%', height: '100%' }}
@@ -19,7 +24,10 @@ const MapDisplay = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <RoutingMachine />
+        <RoutingMachine
+          startingLoc={startingCityLoc}
+          destinationLoc={destinationCityLoc}
+        />
       </StyledMapContainer>
     </Wrapper>
   );
