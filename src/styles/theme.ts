9d33c74ac04883dd11211622cb9d24ac77d9658f
DonquixoteSabo <mixboksu@gmail.com>
2022-05-17 16:49:05 +0200

    chore: Add form button

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ import { DefaultTheme } from 'styled-components';
 const theme: DefaultTheme = {
   colors: {
     primary: '#AEBBFF',
-    primaryHover: '#AEBBFF',
+    primaryHover: '#6d84fe',
     red: '#F40F02',
     redHover: '#b70b01',
   },
