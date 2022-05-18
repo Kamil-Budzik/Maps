f3692127638720a9f4f971dfd80d9c228aca0095
DonquixoteSabo <mixboksu@gmail.com>
2022-05-18 16:49:04 +0200

    feat: Add PDF button

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
-import { Wrapper } from './Map.styles';
 import Sidebar from 'containers/Sidebar';
 import MapDisplay from 'containers/MapDisplay';
 
+import { Wrapper } from './Map.styles';
+
+//TODO there is a problem with react-to-print and react-leaflet.
+// If you have enough time be sure to check it. U might even have to use different tool like 'https://www.youtube.com/watch?v=1Vw-e_ex2S0'
+
 const Map = () => {
+  const componentToPrint = useRef(null);
+
+  const handlePrint = useReactToPrint({
+    content: () => componentToPrint.current,
+  });
+
   return (
-    <Wrapper>
-      <Sidebar />
+    <Wrapper ref={componentToPrint}>
+      <Sidebar handlePrint={handlePrint} />
       <MapDisplay />
     </Wrapper>
   );
