eab5f5f9c0841f84f77eace8f9dcfda8144e6474
DonquixoteSabo <mixboksu@gmail.com>
2022-05-19 11:34:14 +0200

    feat: Add history styles

---
+++ src/pages/History/History.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+export const ContentWrapper = styled.main`
+  border-radius: 25px;
+  padding: 1em 4em 6em;
+  background: white;
+  max-width: 750px;
+  height: 600px;
+  text-align: center;
+
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 1em;
+  }
+
++`;
+
+export const List = styled.ul`
+  margin-bottom: 4em;
+`;
+
+export const Item = styled.li`
+  padding: 1em 2em;
+`;
+
--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Title from 'components/Title';
+import Button from 'components/Button';
+
+import { ContentWrapper, Item, List, Wrapper } from './History.styles';
 
 const History = () => {
   return (
-    <div>
-      <h1>History route</h1>
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <Title />
+        <section>
+          <h2>Historia tras</h2>
+          <List>
+            <Item>
+              <div>Paris, France {'->'} Brzeg, Opolskie</div>
+              <div>500km</div>
+            </Item>
+            <Item>
+              <div>Paris, France {'->'} Brzeg, Opolskie</div>
+              <div>500km</div>
+            </Item>
+          </List>
+          <Link to="/">
+            <Button>Powrót</Button>
+          </Link>
+        </section>
+      </ContentWrapper>
+    </Wrapper>
   );
 };
 
