21198c7d76730d91cd4dd8396de4c87faa351fbe
DonquixoteSabo <mixboksu@gmail.com>
2022-05-31 13:46:06 +0200

    test: Update all tests

--- cypress/integration/Homepage.spec.ts
+++ cypress/integration/Homepage.spec.ts
@@ -4,10 +4,10 @@ describe('Homepage', () => {
   it('Completes the form and renders map', () => {
     cy.visit('/');
     fillInputs();
-    cy.contains('Wyznacz drogę').click();
+    cy.contains('Generate the way').click();
 
-    cy.contains('Spalanie paliwa na 100km').click().type('7');
-    cy.contains('Cena za 1 litr paliwa').click().type('6');
+    cy.contains('Fuel usage for 100km').click().type('7');
+    cy.contains('Fuel price (for 1 liter)').click().type('6');
 
     cy.get('button');
   });
